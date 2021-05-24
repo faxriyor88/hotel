3172e5ea9ae2ca98344b54b10ff61e128a21b6df
FaxriyorUmarxonov <faxriyorumarxonov@gmail.com>
2021-05-24 09:18:36 +0500

    warehouse

--- src/main/java/com/example/hotel/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hotel.repository;
-
-import com.example.hotel.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel,Integer> {
-}
